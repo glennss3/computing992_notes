2b2627982bbdfb2e57dbf02974161b2ecfc825f6
Susan Glenn <glennss3@users.noreply.github.com>
2020-10-08 18:02:12 -0500

    used sed on covariates

--- sed.sh
+++ sed.sh
@@ -1,1 +1,5 @@
+#practice with sed
 sed -E s/([^:]):[0-9]+\.[0-9]+/\1:0\.01/g net.phy
+
+#covariates from Colostrum document
+echo "Treat, Birth, AgeDay, AgeYear, Parity, IA, DryPeriod, DIM, DIM305, TotMilk, Calving, CalvingTime, ColWeight, Brix, IcaCalve, IcaMilk, Breed, Sex" | gsed -E s/,/:::/g
